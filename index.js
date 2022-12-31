4d768554c5e40f6cbbdea5599b57b2fdc70cee5e
StepSkop <74026500+StepSkop@users.noreply.github.com>
2022-12-31 03:52:13 +0100

    Fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const wol = require('wake_on_lan')
 var app = express()
 console.log(process.env.ENV_ONLINE)
 app.post('/',function (req, res) {
-    if (req.headers['acces-token'] === process.env.HOME_API_PC_ON_TOKEN) {
+    if (req.headers['access-token'] === process.env.HOME_API_PC_ON_TOKEN) {
         console.log("GOT!");
         wol.wake(process.env.PC_MAC, { address:process.env.BROADCAST_IP});
         res.json({
